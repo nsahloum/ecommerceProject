b186529d7b084c49b93300bb276f7714e02543fa
nsahloum <sahloumi.najima@gmail.com>
2022-02-26 01:04:36 +0100

    so less code

---
+++ src/main/java/com/fullstack/ecommerceproject/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.fullstack.ecommerceproject.dao;
+
+import com.fullstack.ecommerceproject.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
