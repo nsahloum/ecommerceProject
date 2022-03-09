4b18ba99afa22413310f803e2f8994d28a21da9b
nsahloum <sahloumi.najima@gmail.com>
2022-03-09 21:35:26 +0100

    cross origin

--- src/main/java/com/fullstack/ecommerceproject/dao/ProductRepository.java
+++ src/main/java/com/fullstack/ecommerceproject/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.fullstack.ecommerceproject.dao;
 
 import com.fullstack.ecommerceproject.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200/")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
